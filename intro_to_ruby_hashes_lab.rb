3b0bee93b81a688c6ba2de21cafccfaf228b822c
Eric J Wilhite <ericjwilhite@gmail.com>
2019-06-13 22:33:58 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,17 +1,24 @@
 def new_hash
-  # return an empty hash
+  hash = {}
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  my_hash = {
+    name: "Eric"
+  }
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  hash = {
+    name: "Grace Hopper"
+  }
 end
 
 def id_generator
   # return a hash with a key :id assigned to positive integer
+  hash = {
+    id: 
+  }
 end
 
 def my_hash_creator(key, value)
