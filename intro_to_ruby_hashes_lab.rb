d1c3f103e563a8f6bbd1f299dfd584ed132e7897
Eric J Wilhite <ericjwilhite@gmail.com>
2019-06-13 22:50:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,9 +23,8 @@ end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-  key_sym = key.to_sym
   hash = {
-    key_sym => value
+    key => value
   }
 end
 
